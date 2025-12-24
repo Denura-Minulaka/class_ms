a057483a8b23e8ff42fc3db3e470e78eab0c2d1c
Denura-Minulaka <denura.minulaka@gmail.com>
2025-12-24 12:58:55 +0530

    create the class_ms

---
+++ src/main/java/com/class_ms/ClassMsApplication.java
@@ -0,0 +1,13 @@
+package com.class_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassMsApplication.class, args);
+	}
+
+}
